52125f982881b929fc1aa5f199a42f7e379d1d53
sakamoto024 <s.sakamoto@atomitech.jp>
2019-08-30 09:10:49 +0900

    P33の修正

--- P33/main.py
+++ P33/main.py
@@ -2,10 +2,14 @@ import sys
 sys.path.append('../')
 from P32.main import gcd
 
+#def is_coprime(num1,num2):
+#    gcd_num = gcd(num1,num2)
+#    if gcd_num == 1:
+#        return True
+#    else:
+#        return False
+
+##########################修正コード###########################
+
 def is_coprime(num1,num2):
-    gcd_num = gcd(num1,num2)
-    if gcd_num == 1:
-        return True
-    else:
-        return False
-    
+    return gcd(num1,num2) == 1
